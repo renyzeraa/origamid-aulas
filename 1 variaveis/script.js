82a9bdd9e0e85f35402c831c85cb4def8cbed580
renan <renansilvaytb@gmail.com>
2022-06-25 16:40:21 -0300

    variaveis

---
+++ 1 variaveis/script.js
@@ -0,0 +1,29 @@
+// VARIAVEIS
+
+// var nome = "Renan"
+//  var idade = 23
+//  var faculdade = true
+
+//  console.log(nome, idade, faculdade)
+
+// OPERAÇÕES MATEMATICAS
+
+// var preco = 25
+// var quantidade = 5
+// var precoTotal = preco * quantidade
+//  console.log(precoTotal)
+
+//                  exercicio
+// Declarar uma variável com o seu nome
+let meuNome = 'Renan'
+// Declarar uma variável com a sua idade
+let minhaIdade = 23
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'pizza'
+// Declarar 5 variáveis diferentes sem valores
+var cachorro, gato, pato, casa, eu
+
+console.log(meuNome, minhaIdade, comidaFavorita)
