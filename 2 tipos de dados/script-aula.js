46e02d4fb95fd1093a0f85ed37043da1e7b87ebb
renan <renansilvaytb@gmail.com>
2022-06-25 17:09:46 -0300

    fim da aula 2

--- 2 tipos de dados/script-aula.js
+++ 2 tipos de dados/script-aula.js
@@ -1,15 +1,30 @@
+let nome = 'Renan'
+let idade = 23
+let time = null
+
+console.log(typeof time)
+
+let sobrenome = 'silva'
+let nomeCompleto = nome + ' ' + sobrenome
+
+console.log(nomeCompleto)
+
+var gols = 900
+var frase1 = 'Romario fez ' + gols + ' na carreira'
+var frase2 = `Romario fez ${gols} na carreira`
+
+console.log(frase1)
+console.log(frase2)
+
 // Declare uma variável contendo uma string
-var nome = 'Renan'
+
 // Declare uma variável contendo um número dentro de uma string
-var ano = '2022'
+
 // Declare uma variável com a sua idade
-var idade = 23
+
 // Declare duas variáveis, uma com seu nome
 // e outra com seu sobrenome e some as mesmas
-var nome = 'renan'
-var sobrenome = 'silva'
-var fraseNome = `${nome} ${sobrenome}`
+
 // Coloque a seguinte frase em uma variável: It's time
-var frase1 = "It's time"
+
 // Verifique o tipo da variável que contém o seu nome
-console.log(nome)
