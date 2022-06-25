86bb665ae91ccbde154f1166ff9e28e1ed118602
renan <renansilvaytb@gmail.com>
2022-06-25 18:30:42 -0300

    fim aula 3

--- 3-numeros-e-operadores/script.js
+++ 3-numeros-e-operadores/script.js
@@ -1,5 +1,18 @@
-var idade = 23
-var gols = 11
-var exponencial = 2e8
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20
+console.log(total)
+// Crie duas expressões que retornem NaN
+var fraseNaN = 'Luiz 20' - 10
+console.log(fraseNaN)
+// Somar a string '200' com o número 50 e retornar 250
+var somaString = +'200' + 50
+console.log(somaString)
+// Incremente o número 5 e retorne o seu valor incrementado
+var numero5 = 5
+console.log(++numero5)
+// Como dividir o peso por 2?
+var numero = +'80' / 2
+var unidade = 'kg'
+var peso = numero + unidade // '80kg'
 
-console.log('oi')
+console.log(peso)
