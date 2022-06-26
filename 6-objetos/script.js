d6aca0a08c62d87a6fd7069dc18b4e3baeff3e55
renan <renansilvaytb@gmail.com>
2022-06-26 14:51:05 -0300

    fim aula 6

--- 6-objetos/script.js
+++ 6-objetos/script.js
@@ -1,9 +1,35 @@
+// Crie um objeto com os seus dados pessoais
+// Deve possuir pelo menos duas propriedades nome e sobrenome
 var pessoa = {
-  nome: 'André',
-  idade: 28,
-  profissao: 'Designer',
-  possuiFaculdade: true
+  nome: 'renan',
+  sobrenome: 'silva',
+  idade: 23
 }
 
-console.log(pessoa.nome) // 'André'
-pessoa.possuiFaculdade // true
+// Crie um método no objeto anterior, que mostre o seu nome completo
+pessoa.nomeCompleto = `${pessoa.nome} ${pessoa.sobrenome}`
+console.log(pessoa.nomeCompleto.toUpperCase())
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi'
+}
+carro.preco = 3000
+
+console.log(carro)
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+var cachorro = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === 'homem') {
+      console.log('latindo')
+    } else {
+      console.log('quieto')
+    }
+  }
+}
