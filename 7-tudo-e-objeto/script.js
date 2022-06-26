4a75290e9f7db46c1e2a7671e3b0947e11be81f6
renan <renansilvaytb@gmail.com>
2022-06-26 15:30:49 -0300

    fim aula 7

--- 7-tudo-e-objeto/script.js
+++ 7-tudo-e-objeto/script.js
@@ -1,41 +1,16 @@
-// TUDO É OBJETO
-// Strings, Números, Boolean, Objetos e mais, possuem propriedades e métodos. Por isso são objetos.
-var nome = 'André'
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'renan'
+// charAt()
+// length
+// toUpperCase()
 
-nome.length // 5
-nome.charAt(1) // 'n'
-nome.replace('ré', 'rei') // 'Andrei'
-nome // 'André'
-// Uma string herda propriedades e métodos do construtor String()
-
-// NÚMEROS
-var altura = 1.8
-
-altura.toString() // '1.8'
-altura.toFixed() // '2'
-// Por um breve momento o número é envolvido em um Objeto (coerção), tendo acesso assim as suas propriedades e métodos
-
-// FUNÇÕES
-function areaQuadrado(lado) {
-  return lado * lado
-}
-
-areaQuadrado.toString()
-//"function areaQuadrado(lado) {
-//  return lado * lado;
-//}"
-
-areaQuadrado.length // 1
-
-// ELEMENTOS DO DOM
+// nomeie 5 propriedades ou métodos de elementos do DOM
 var btn = document.querySelector('.btn')
+// innerHTML
+// addEventListener()
+// classList
+// textContent
+// id
 
-btn.classList.add('azul') // adiciona a classe azul
-btn.innerText // 'Clique'
-btn.addEventListener('click', function () {
-  console.log('Clicou')
-})
-// Praticamente todos os efeitos com JS são feitos utilizando propriedades e métodos de objetos do DOM.
-
-// OBJETOS REVOLUCIONARAM A PROGRAMAÇÃO
-// Web API's são métodos e propriedades que permitem a interação JavaScript e Browser.
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
