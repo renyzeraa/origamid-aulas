d56c8a389539c938888405f26b22f5610ff90321
renan <renansilvaytb@gmail.com>
2022-06-27 17:58:05 -0300

    novas pastas

---
+++ javascript-es6/02-dom-para-iniciantes/01-dom/script.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+const hrefPagina = window.location.href
+console.log(hrefPagina)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo')
+console.log(elementoAtivo)
+
+// Retorne a linguagem do navegador
+const languagePage = window.navigator.language
+console.log(languagePage)
+
+// Retorne a largura da janela
+const larguraJanela = window.innerWidth
+console.log(larguraJanela)
