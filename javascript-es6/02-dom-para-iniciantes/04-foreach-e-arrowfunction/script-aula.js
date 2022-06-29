36e6685e856293823868cda890f432abad853f35
renan <renansilvaytb@gmail.com>
2022-06-28 21:55:30 -0300

    arrumando conteudo

--- javascript-es6/02-dom-para-iniciantes/04-foreach-e-arrowfunction/script-aula.js
+++ javascript-es6/02-dom-para-iniciantes/04-foreach-e-arrowfunction/script-aula.js
@@ -2,7 +2,7 @@
 // Constantemente vamos selecionar uma lista de itens do dom. A melhor forma para interagirmos com os mesmos é utilizando o método forEach.
 const imgs = document.querySelectorAll('img')
 
-imgs.forEach(function (item) {
+imgs.forEach(item => {
   console.log(item)
 })
 
