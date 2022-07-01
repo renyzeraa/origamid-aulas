e766dbc6d6bf9da6b6a2e96caa1d548b121cca13
renan <renansilvaytb@gmail.com>
2022-07-01 15:35:33 -0300

    exercicio01 completo

--- javascript-es6/03-objetos/01-constructor-functions/script.js
+++ javascript-es6/03-objetos/01-constructor-functions/script.js
@@ -7,8 +7,19 @@ const pessoa = {
   }
 }
 
+function Pessoa(nome, idade) {
+  this.idade = idade
+  this.andar = () => {
+    console.log(`${nome} andou'`)
+  }
+}
+const pessoa1 = new Pessoa('Renan', 23)
+
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
+const joao = new Pessoa('João', 20)
+const maria = new Pessoa('Maria', 25)
+const bruno = new Pessoa('Bruno', 15)
 
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
@@ -17,3 +28,20 @@ const pessoa = {
 // elements, retorna NodeList com os elementos selecionados
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
+function Dom(seletor) {
+  const elementList = document.querySelectorAll(seletor)
+  this.element = elementList
+  this.addClass = classe => {
+    elementList.forEach(element => {
+      element.classList.add(classe)
+    })
+  }
+  this.removeClass = classe => {
+    elementList.forEach(element => {
+      element.classList.remove(classe)
+    })
+  }
+}
+
+const listaItens = new Dom('li')
+listaItens.addClass('ativo')
