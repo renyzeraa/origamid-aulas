529dfbb967e3f4f1bd567cd349b04cca41d69d56
renan <renansilvaytb@gmail.com>
2022-07-02 16:05:33 -0300

    prototype ✔️

--- javascript-es6/03-objetos/02-prototype/script.js
+++ javascript-es6/03-objetos/02-prototype/script.js
@@ -1,1 +1,36 @@
-function teste() {}
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoas(nome, sobrenome, idade) {
+  this.nome = nome
+  this.sobrenome = sobrenome
+  this.idade = idade
+}
+
+Pessoas.prototype.nomeCompleto = () => {
+  return `${this.nome} ${this.sobrenome}`
+}
+
+const renan = new Pessoas('Renan', 'Silva', 23)
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+Object.getOwnPropertyNames(NodeList.prototype)
+Object.getOwnPropertyNames(HTMLCollection.prototype)
+Object.getOwnPropertyNames(Document.prototype)
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li')
+
+li // 'HTMLLIElement'
+li.click // 'Function'
+li.innerText // 'String'
+li.value // 'Number'
+li.hidden // 'Boolean'
+li.offsetLeft // 'Number'
+li.click() // undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name // string
