a2400a249f551d0a535c5b8e788f81bd4840a47f
renan <renansilvaytb@gmail.com>
2022-07-02 17:05:00 -0300

    prototype readme

--- javascript-es6/03-objetos/03-native-host-user/script.js
+++ javascript-es6/03-objetos/03-native-host-user/script.js
@@ -1,6 +1,18 @@
 // Liste 5 objetos nativos
+Object
+Function
+Number
+Array
+String
 
 // Liste 5 objetos do browser
+Window
+History
+Documento
+HTMLAllCollection
+HTMLBodyElement
 
 // Liste 2 Métodos, Propriedades ou Objetos
 // presentes no Chrome que não existem no Firefox
+
+// metodos de webkit
