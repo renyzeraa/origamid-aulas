df787866ebdde71edf87447e580ee7d88721ebc7
renan <renansilvaytb@gmail.com>
2022-07-06 16:34:35 -0300

    fim aula array

---
+++ javascript-es6/03-objetos/06-array/script.js
@@ -0,0 +1,42 @@
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão']
+// Remova o primeiro valor de comidas e coloque em uma variável
+// Remova o último valor de comidas e coloque em uma variável
+// Adicione 'Arroz' ao final da array
+// Adicione 'Peixe' e 'Batata' ao início da array
+const primeiroValor = comidas.shift()
+const ultimoValor = comidas.pop()
+comidas.push('Arroz')
+comidas.unshift('Peixe', 'Batata')
+console.log(primeiroValor, ultimoValor)
+console.log(comidas)
+
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia']
+// Arrume os estudantes em ordem alfabética
+estudantes.sort()
+// Inverta a ordem dos estudantes
+estudantes.reverse()
+// Verifique se Joana faz parte dos estudantes
+console.log(estudantes.includes('Joana'))
+// Verifique se Juliana faz parte dos estudantes
+console.log(estudantes.includes('Juliana'))
+
+// Substitua section por ul e div com li,
+// utilizando split e join
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+html = html.split('section').join('ul').split('div').join('li')
+console.log(html)
+
+//console.log(newHtml)
+
+const carros = ['Ford', 'Fiat', 'VW', 'Honda']
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const carrosCopia = carros.slice()
+carros.pop()
+
+console.log(carrosCopia)
+console.log(carros)
