4146b846a593f504ac8566b9dc7fb7b5893cc364
renyzeraa <renansilvaytb@gmail.com>
2023-03-20 19:29:45 -0300

    modulo-10

--- js-jquery/03-jquery/3-10-animacao-com-scroll-mini_arquivos/vanbook-inicio/js/main.js
+++ js-jquery/03-jquery/3-10-animacao-com-scroll-mini_arquivos/vanbook-inicio/js/main.js
@@ -1,7 +1,20 @@
-// Dica: Quebre em pequenas partes, procure códigos semelhantes e teste.
+/* 
+Dica: Quebre em pequenas partes, procure códigos semelhantes e teste.
 
-// 1 - Distância entre o scroll e o topo
-// 2 - Ditância entre o elemento e o topo
-// 3 - Verificar essas duas variáveis sempre que o scroll for ativado
-// 4 - Se distância entre o elemento e o topo for maior que do scroll, adicionar classe
-// 5 - A classe deve mostrar e animar o elemento
+1 - Distância entre o scroll e o topo
+2 - Distância entre o elemento e o topo
+3 - Verificar essas duas variáveis sempre que o scroll for ativado
+4 - Se distância entre o elemento e o topo for maior que do scroll, adicionar classe
+5 - A classe deve mostrar e animar o elemento
+*/
+
+$(window).scroll(function () {
+    var iWinTop = $(this).scrollTop()
+    $('.anime').each(function () {
+        if (iWinTop > $(this).offset().top - 400) {
+            $(this).addClass('anime-init')
+        } else {
+            $(this).removeClass('anime-init')
+        }
+    })
+})
