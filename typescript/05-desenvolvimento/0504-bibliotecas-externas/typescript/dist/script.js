141f241cf41042e1f9f4cbdb11988d77427ebb1e
Renan Silva <renansilvaytB@gmail.com>
2023-09-20 18:33:09 -0300

    feat modulo 05

---
+++ typescript/05-desenvolvimento/0504-bibliotecas-externas/typescript/dist/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const body = $('body');
+body.addClass('ativo');
+_.intersection([1, 2, 3, 5, 6], [2, 3, 1, 9]);
+const iframe = document.getElementById('vimeo');
+const player = new Vimeo.Player(iframe);
+console.log(player);
+//# sourceMappingURL=script.js.map
