08348dff27f2ef1072e8be95813e72c5bc5be5e4
Renan Silva <renansilvaytB@gmail.com>
2023-09-20 18:32:44 -0300

    feat modulo 03

---
+++ typescript/03-objetos-e-interfaces/0302-interfaces-do-dom/typescript/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const links = document.querySelectorAll(".link");
+console.log(links);
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        console.log(link.href);
+    }
+});
+const dados = [1, 2, 3];
+const arrayLinks = Array.from(links);
+const anchorLinks = arrayLinks.filter((link) => link instanceof HTMLAnchorElement);
+console.log(anchorLinks);
