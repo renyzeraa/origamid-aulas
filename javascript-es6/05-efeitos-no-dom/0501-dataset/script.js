eeafd3ed9c1642bd37d753b0c203c94d10f0b1cb
Renan Silva <renansilvaytB@gmail.com>
2023-10-10 09:03:22 -0300

    05 - dataset ✅

--- javascript-es6/05-efeitos-no-dom/0501-dataset/script.js
+++ javascript-es6/05-efeitos-no-dom/0501-dataset/script.js
@@ -2,6 +2,8 @@
 // data-anime="show-right" a todos as section's
 // com descricão dos animais.
 
+
+
 // Utilizando estes atributos, adicione a classe
 // show-down ou show-right a sua respectiva section
 // assim que a mesma aparecer na tela (animacao tab)
