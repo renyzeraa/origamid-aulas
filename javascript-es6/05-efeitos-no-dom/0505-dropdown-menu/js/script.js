bd3e34f33b43ba7fef9ccd7dde8e5100f7127a8f
Renan Silva <renansilvaytB@gmail.com>
2023-10-10 15:56:59 -0300

    initial commit

--- javascript-es6/05-efeitos-no-dom/0505-dropdown-menu/js/script.js
+++ javascript-es6/05-efeitos-no-dom/0505-dropdown-menu/js/script.js
@@ -3,8 +3,7 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
+import initTooltip from './modules/tooltip.js'
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -12,4 +11,3 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
-initDropdownMenu();
