21875f1f7a0a85ff0750cd539449d326e92d94d4
Renan Silva <renansilvaytB@gmail.com>
2023-10-13 16:37:55 -0300

    initial commit

--- javascript-es6/05-efeitos-no-dom/0511-date-object/js/script.js
+++ javascript-es6/05-efeitos-no-dom/0511-date-object/js/script.js
@@ -6,8 +6,7 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
-import initFuncionamento from './modules/funcionamento.js';
+import initNumerosAnimation from './modules/numeros-animacao.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +16,4 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
-initFuncionamento();
+initNumerosAnimation();
