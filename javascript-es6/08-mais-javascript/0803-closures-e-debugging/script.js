0110e9124a1449c0e8fdcd1cdf7f7b384a8f5bc8
Renan Silva <renansilvaytB@gmail.com>
2023-11-07 18:52:56 -0300

    03 - closures e debugging

---
+++ javascript-es6/08-mais-javascript/0803-closures-e-debugging/script.js
@@ -0,0 +1,30 @@
+// debugger; // para aqui instantaneamente, caso o devtools esteja aberto
+
+let item1 = 1; 
+function funcao1() { 
+  let item2 = 2;  // clojures , como se fosse uma mochila que tem acesso ao que esta aqui dentro
+  function funcao2() { 
+    let item3 = 3;   // primeiro escopo
+    console.log(item1); 
+    console.log(item2); 
+    console.log(item3); 
+  } 
+  funcao2(); 
+} 
+funcao1()
+
+/* -------- */
+
+function contagem() {
+  let total = 0;
+  return incremento = () => {
+    total++;
+    console.log(total);
+    return total;
+  }
+}
+
+const ativarIncremento = contagem();
+ativarIncremento();
+
+// setInterval(ativarIncremento, 500);
